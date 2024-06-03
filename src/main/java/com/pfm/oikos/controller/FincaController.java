db35734b61c98a782dec3789ae4677119c6d4d5b
sergiohut <sergiosantiagocarmona@gmail.com>
2024-06-03 18:12:17 +0200

    getAllFincas added to Finca Controller

--- src/main/java/com/pfm/oikos/controller/FincaController.java
+++ src/main/java/com/pfm/oikos/controller/FincaController.java
@@ -1,5 +1,7 @@
 package com.pfm.oikos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -53,4 +55,9 @@ public class FincaController {
     }
   }
 
+  @GetMapping
+  public ResponseEntity<List<Finca>> getAllFincas() {
+      List<Finca> fincas = fincaService.getAllFincas();
+      return new ResponseEntity<>(fincas, HttpStatus.OK);
+  }
 }
