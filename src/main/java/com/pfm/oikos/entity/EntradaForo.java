6f30c3b34f3d4100f6ab0bf123192c43d609cf48
Daniel Walkington <danielwalkingtonesp@gmail.com>
2024-06-11 19:47:56 +0200

    Adecuar entidad EntradaForo a tabla SQL Entrada

--- src/main/java/com/pfm/oikos/entity/EntradaForo.java
+++ src/main/java/com/pfm/oikos/entity/EntradaForo.java
@@ -1,7 +1,7 @@
 package com.pfm.oikos.entity;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,8 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,26 +19,14 @@ public class EntradaForo {
     private Integer idEntrada;
 
     @ManyToOne
-    @JoinColumn(name = "idHilo")
-    private HiloForo hiloForo;
-
-    @ManyToOne
     @JoinColumn(name = "idAutor")
     private Usuario autor;
 
-    private String nombre;
     private String titulo;
     private String textoComentario;
     private LocalDate fecha;
     private LocalTime hora;
 
-    @OneToMany(mappedBy = "entradaForo")
-    private Set<RespuestaForo> respuestas;
-
-    @OneToOne(mappedBy = "entradaForo")
-    private Encuesta encuesta;
-
-    // Getters and Setters
     public Integer getIdEntrada() {
         return idEntrada;
     }
@@ -49,28 +35,12 @@ public class EntradaForo {
         this.idEntrada = idEntrada;
     }
 
-    public HiloForo getHiloForo() {
-        return hiloForo;
-    }
-
-    public void setHiloForo(HiloForo hiloForo) {
-        this.hiloForo = hiloForo;
-    }
-
     public Usuario getAutor() {
         return autor;
     }
 
     public void setAutor(Usuario autor) {
         this.autor = autor;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getTitulo() {
@@ -104,29 +74,4 @@ public class EntradaForo {
     public void setHora(LocalTime hora) {
         this.hora = hora;
     }
-
-    public Set<RespuestaForo> getRespuestas() {
-        return respuestas;
-    }
-
-    public void setRespuestas(Set<RespuestaForo> respuestas) {
-        this.respuestas = respuestas;
-    }
-
-    public Encuesta getEncuesta() {
-        return encuesta;
-    }
-
-    public void setEncuesta(Encuesta encuesta) {
-        this.encuesta = encuesta;
-    }
-
-    public Object getContenido() {
-        throw new UnsupportedOperationException("Unimplemented method 'getContenido'");
-    }
-
-    public void setContenido(Object contenido) {
-        throw new UnsupportedOperationException("Unimplemented method 'setContenido'");
-    }
 }
-
