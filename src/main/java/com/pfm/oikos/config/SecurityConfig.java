13d58a5c415c8dbefc8d853a56689d6841e1441d
sergiohut <sergiosantiagocarmona@gmail.com>
2024-06-20 15:45:45 +0200

    crud de registros completado

--- src/main/java/com/pfm/oikos/config/SecurityConfig.java
+++ src/main/java/com/pfm/oikos/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
                         		.requestMatchers(HttpMethod.POST, "/api/v1/instalaciones/**").hasAuthority("ADMIN")  // Require ADMIN role for POST to /api/v1/instalaciones/
                                 .requestMatchers(HttpMethod.DELETE, "/api/v1/instalaciones/**").hasAuthority("ADMIN")      
                                 .requestMatchers(HttpMethod.PATCH, "/api/v1/instalaciones/**").hasAuthority("ADMIN")   
-                                .requestMatchers(HttpMethod.PATCH, "/api/v1/usuarios/**\"").hasAuthority("ADMIN")                    
+                                .requestMatchers(HttpMethod.PATCH, "/api/v1/usuarios/**\"").hasAuthority("ADMIN")       
+                                .requestMatchers(HttpMethod.DELETE, "/api/v1/usuarios/**\"").hasAuthority("ADMIN")                    
                                 .requestMatchers("/admin_only/**").hasAuthority("ADMIN")
                                 .anyRequest()
                                 .authenticated()
