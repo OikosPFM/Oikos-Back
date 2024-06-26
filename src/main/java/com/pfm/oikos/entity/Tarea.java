2e05ee61e5703d60d03c447f90fe5f179bca320d
Daniel Walkington <danielwalkingtonesp@gmail.com>
2024-06-26 19:36:42 +0200

    actualización tarea entity

--- src/main/java/com/pfm/oikos/entity/Tarea.java
+++ src/main/java/com/pfm/oikos/entity/Tarea.java
@@ -13,9 +13,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tarea")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tarea {
 
     @Id
