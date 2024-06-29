09753f4c0a928d3181e43e64b7a96548af5508ff
Daniel Walkington <danielwalkingtonesp@gmail.com>
2024-06-29 09:15:51 +0200

    imports sin usar fuera

--- src/main/java/com/pfm/oikos/repository/TareaRepository.java
+++ src/main/java/com/pfm/oikos/repository/TareaRepository.java
@@ -1,6 +1,9 @@
 package com.pfm.oikos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.pfm.oikos.entity.Tarea;
